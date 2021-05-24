18fc31d32350cbde78a84f3b80f870848daddd21
Leo <83550516+Leopoldo-Flores@users.noreply.github.com>
2021-05-23 23:36:22 -0700

    Add files via upload

---
+++ assignment2/script.js
@@ -0,0 +1,34 @@
+let Product = prompt('Enter the Product Name: ');
+let Price = prompt('Enter the Product Price: ');
+
+let TotalPrice = function(Price){
+    return Number(Price);
+}
+let CaliforniaTax = function(price, Tax){
+    return (Number(Tax)*Number(price))/100;
+}
+let Tax = 7.25;
+let price = TotalPrice(Price);
+let tax = CaliforniaTax(price, Tax);
+let finalPrice = price + tax; 
+
+document.write(`<div>
+            <table>
+                <tr>
+                    <th>Product Name</th>
+                    <td>${Product}</td>
+                </tr>
+                <tr>
+                    <th>Product Price</th>
+                    <td>$${Price}</td>
+                </tr>
+                <tr>
+                    <th>Tax</th>
+                    <td>$${tax}</td>
+                </tr>
+                <tr class="select-total">
+                    <th>Final Price</th>
+                    <td>$${finalPrice}</td>
+                </tr>
+                </table> 
+            </div>`);
